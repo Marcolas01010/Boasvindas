27aa516f2b3000e879ca82484b1943aab5b43a3f
Marcolas01010 <shark06901@sharkcoders.pt>
2024-11-23 12:56:37 +0000

    Add files via upload

---
+++ While.py
@@ -0,0 +1,32 @@
+#numero = int (input ("Qual numero vai querer ?"))
+#soma = 0
+#while numero >= 0:
+
+    #soma = soma+numero
+# numero = int(input("Escolhe outro  numero"))
+#print(soma)
+#for x in "Sharkcoders":
+    #print(x)
+    #if x == "k":
+        #break
+#"for x in range(2,20,2) :
+    #print(x)
+numero_final= int(input("Escolhe um numero"))
+numero_inicial =int(input("Qual valor queres pro 0 :"))
+saltar= int(input("Escolha um numero para saltar"))
+
+
+print ("Os numeros são :")
+for i in  range (1,numero_final+1,1) :
+    if i % 2 == 0:
+        print(i)
+
+
+
+
+
+
+
+
+
+
