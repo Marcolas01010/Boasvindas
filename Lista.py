11be7672e10d8e122ae3631500d03e41850f18fc
Marcolas01010 <shark06901@sharkcoders.pt>
2024-11-30 13:04:08 +0000

    Add files via upload

---
+++ Lista.py
@@ -0,0 +1,9 @@
+lista=["Portugal","Brasil","Inglaterra","Alemanha","Russia"]
+pais= input("Qual país gostaria ?")
+if pais in lista :
+    print("Ok,esse país está na lista, pode viajar.")
+if pais not in lista :
+    print("Esse país não é permitido viajar com esse passaporte")
+
+
+
